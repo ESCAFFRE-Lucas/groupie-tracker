55864617882b62ce61cb546afd9f99d9a44e547e
LESCAFFRE <lucas.escaffre@ynov.com>
2022-01-31 17:01:51 +0100

    get the relations from the URl

--- main.go
+++ main.go
@@ -1,13 +1,29 @@
 package main
 
 import (
+	"fmt"
 	"groupie-tracker/Artists"
-	"groupie-tracker/Dates"
 	"groupie-tracker/Relation"
+	"html/template"
+	"net/http"
 )
 
-func main() {
-	Artists.GetArtists()
-	Dates.GetDates()
+func HomePage(w http.ResponseWriter, r *http.Request) {
 	Relation.GetRelations()
+	tmpl := template.Must(template.ParseFiles("index.gohtml"))
+	data := Artists.GetArtists()
+	_ = tmpl.Execute(w, data)
+
+}
+
+func main() {
+	server := http.NewServeMux()
+
+	server.HandleFunc("/", HomePage)
+
+	server.Handle("/public/", http.StripPrefix("/public/", http.FileServer(http.Dir("./public"))))
+	// listen to the port 8000
+	fmt.Println("server listening on http://localhost:8000/")
+
+	_ = http.ListenAndServe(":8000", server)
 }
