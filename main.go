2c945ac7544a6371108bd3f49da493718c4acde9
LESCAFFRE <lucas.escaffre@ynov.com>
2022-02-04 11:45:02 +0100

    creation of a template directory and a gohtml file for 1 artist, refactor the other gohtml file and print 1 artist on the website with template

--- main.go
+++ main.go
@@ -5,11 +5,10 @@ import (
 	"groupie-tracker/artists"
 	"html/template"
 	"net/http"
-	"strings"
 )
 
 func HomePage(w http.ResponseWriter, r *http.Request) {
-	tmpl := template.Must(template.ParseFiles("index.gohtml"))
+	tmpl := template.Must(template.ParseFiles("templates/index.gohtml"))
 	data := artists.GetArtists()
 	err := tmpl.Execute(w, data)
 	if err != nil {
@@ -18,19 +17,10 @@ func HomePage(w http.ResponseWriter, r *http.Request) {
 }
 
 func ArtistPage(w http.ResponseWriter, r *http.Request) {
-	tmpl := template.Must(template.ParseFiles("artist-index.gohtml"))
+	tmpl := template.Must(template.ParseFiles("templates/artist.gohtml"))
 	fmt.Println(r.URL.Path)
-	id := r.URL.Path[len("/artist/"):]
-	if strings.Contains(id, "/") {
-		w.WriteHeader(http.StatusNotFound)
-		_, _ = w.Write([]byte("degage en*****"))
-		return
-	}
-	_, err := w.Write([]byte(id))
-	if err != nil {
-		fmt.Println(err)
-	}
-	err = tmpl.Execute(w, id)
+	artist := artists.GetArtist()
+	err := tmpl.Execute(w, artist)
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -40,8 +30,8 @@ func ArtistPage(w http.ResponseWriter, r *http.Request) {
 func main() {
 	server := http.NewServeMux()
 
-	server.HandleFunc("/artist", HomePage)
-	server.HandleFunc("/artist/", ArtistPage)
+	server.HandleFunc("/artist", HomePage)    //Every artist
+	server.HandleFunc("/artist/", ArtistPage) //Single artist
 
 	server.Handle("/public/", http.StripPrefix("/public/", http.FileServer(http.Dir("./public"))))
 	// listen to the port 8000
