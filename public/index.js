e1d0832d1e028b55241121b06f3f30b51c850fe8
LESCAFFRE <lucas.escaffre@ynov.com>
2022-02-11 17:08:53 +0100

    try to use google maps API, finish the search bar, formatting the page, and try to change the fonts of the text, creation of the js file

---
+++ public/index.js
@@ -0,0 +1,28 @@
+// function initMap() {
+//     const map = new google.maps.Map(document.getElementById("map"), {
+//         center: { lat: 40.76, lng: -73.983 },
+//         zoom: 15,
+//         mapTypeId: "satellite",
+//     });
+//
+//     map.setTilt(45);
+// }
+
+function searchMenu() {
+    // Declare variables
+    const input = document.getElementById("mySearch");
+    const filter = input.value.toUpperCase();
+    // ul = document.getElementsByClassName("name");
+    // li = ul.getElementsByTagName("li")
+    const listItems = document.querySelectorAll(".name li")
+
+    // Loop through all list items, and hide those who don't match the search query
+    for ( let i = 0; i < listItems.length; i++) {
+        let a = listItems[i].getElementsByTagName("a")[0];
+        if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+            listItems[i].style.display = "";
+        } else {
+            listItems[i].style.display = "none";
+        }
+    }
+}
